2960e3abfc7a510ffdf4181eab1967250ab9d57f
G.Ambatte <sean@robertson-family.nz>
2018-10-30 23:29:14 +1300

    Sequence detection working! Current maximum sequence length is 10, but adjustable via accelLength. Compiles to 86% of available space of a GEMMA microcontroller.

--- gemma_accel.ino
+++ gemma_accel.ino
@@ -1,15 +1,22 @@
 #include <TinyWireM_MMA8451.h>
-#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h>
 
 float now = 0;
 int dlyAmt = 1000;
 int flashDelay = 100;
 
 int ledPin = 1;
-int pixelCount = 16;
+//int pixelCount = 16;
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(pixelCount, ledPin, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(pixelCount, ledPin, NEO_GRB + NEO_KHZ800);
+
+const int seqLength = 3;
+int seq[seqLength] = {1, 2, 3};
+
+int currentState = 0;
+const int accelLength = 10;
+int accelState[accelLength] = {0,0,0,0,0,0,0,0,0,0};
 
 void setup() {
   // Configure the LED pin as an output
@@ -21,15 +28,15 @@ void setup() {
   delay(dlyAmt);
 
   // Check for accelerometer connection
-  while (!mma.begin()) {
-    // If no accelerometer, flash 10 times
+  while (!mma.begin(0x1D)) {
+    // If no accelerometer at default address, flash 10 times
     flash(10);
     // Wait before trying again
     delay(dlyAmt);
   }
 
   // Initialise Pixels library
-  pixels.begin();
+//  pixels.begin();
 
   // Set start time
   now = millis();
@@ -56,19 +63,39 @@ void loop() {
     x = abs(x);
     y = abs(y);
     z = abs(z);
-
-    // Set the number of flashes to 0
-    int flashCount = 0;
-    // Set number of flashes by largest acceleration:
-    // 1 flash if X has largest acceleration
-    // 2 flashes if Y has largest acceleration
-    // 3 flashes if Z has largest acceleration
+   
+    // Set state by largest acceleration:
+    // = 1 if X has largest acceleration
+    // = 2 if Y has largest acceleration
+    // = 3 if Z has largest acceleration
+    // State definition is arbitrary and whatever we set it to
+    // However the more states we have, the more likely we are to hit incorrect states and fail to execute a sequence
     
-    if (max3(x, y, z) == x) { flashCount = 1; };
-    if (max3(x, y, z) == y) { flashCount = 2; };
-    if (max3(x, y, z) == z) { flashCount = 3; };
+    if (max3(x, y, z) == x) { currentState = 1; };
+    if (max3(x, y, z) == y) { currentState = 2; };
+    if (max3(x, y, z) == z) { currentState = 3; };
 
-    flash(flashCount);
+    // If state has changed
+    if ( currentState != accelState[0] ) {
+      // Update the previous states
+      // For each item in the accelState array, starting at the second to last item
+      // and moving back through the list to the first item
+      for (int i = accelLength-1; i>=0; i--) {
+        // Set the next item in the array to the value of this item
+        accelState[i+1] = accelState[i];
+      }
+      // Update the first item in the array to the current state
+      accelState[0] = currentState;
+
+      // Compare the latest part of the accelStates array to the seq array
+      if ( array_compare(accelState, seq, seqLength) ) {
+        // If they match, show success indication
+        magicPixelSeq();
+      }
+    }
+    
+    flash(currentState);
+//    updatePixels();
   }
 }
 
@@ -85,13 +112,30 @@ float max3(float a, float b, float c) {
   return max(max(a,b),c);
 }
 
-void updatePixels(void) {
-    for(int i=0; i<pixelCount ;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
+boolean array_compare(int *array_a, int *array_b, int array_len){
+  // Compare array elements. If different, return false
+  for ( int i = 0; i<array_len; i++ ) {
+    if ( array_a[i] != array_b[i] ) {
+      return false;
+    }
   }
+  return true;
 }
+
+//void updatePixels(void) {
+//    for(int i=0; i<pixelCount ;i++){
+//
+//    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+//    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+//
+//    pixels.show(); // This sends the updated pixel color to the hardware.
+//  }
+//}
+
+void magicPixelSeq(void) {
+  // Stub for visual effect on success
+  flash(6);
+  // updatePixels();
+}
+
 
